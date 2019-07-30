c9f30bdc0af2aa88e6993425fadce547b0b3a91d
ISMAIL RAMZAN <m-ismail-ramzan@yahoo.com>
2019-07-30 16:50:03 +0500

    Updates

---
+++ precache-manifest.fc733c710330681ce74657031e52a411.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83cd315483f0eaebf4cca481054b2ab2",
+    "url": "/Currency-App/index.html"
+  },
+  {
+    "revision": "a826eff545603f4ea17b",
+    "url": "/Currency-App/static/css/main.429357ba.chunk.css"
+  },
+  {
+    "revision": "511a6eb6b7ec2e1e4ec3",
+    "url": "/Currency-App/static/js/2.26ba37d3.chunk.js"
+  },
+  {
+    "revision": "a826eff545603f4ea17b",
+    "url": "/Currency-App/static/js/main.45ad692f.chunk.js"
+  },
+  {
+    "revision": "899666033b0b8cbaa68b",
+    "url": "/Currency-App/static/js/runtime~main.bfe1040f.js"
+  }
+]);
